04485311acfde9a3804f8c7956296cdaa93fdf44
Sahil07021997 <sahilkumardhiman07@gmail.com>
2025-08-03 15:03:44 +0530

    implementation - graphs & binary search

--- dsa/binary_search.py
+++ dsa/binary_search.py
@@ -1,3 +1,27 @@
-if __name__ == "__main__":
-    """This script is designed to run a practice session in Python."""
-    pass
+def binary_search(arr, target):
+    """
+    Perform binary search on a sorted array to find the index of the target value.
+
+    :param arr: List of sorted integers.
+    :param target: Integer value to search for.
+    :return: Index of target in arr if found, otherwise -1.
+    """
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 9
+res_index = binary_search(arr, target)
+print(f"Index of {target} in the array: {res_index}")
--- dsa/graphs.py
+++ dsa/graphs.py
@@ -1,3 +1,59 @@
-if __name__ == "__main__":
-    """This script is designed to run a practice session in Python."""
-    pass
+from collections import defaultdict
+
+
+class DirectedGraph:
+    """A class to represent a directed graph using an adjacency list."""
+
+    def __init__(self):
+        """Initialize an empty graph."""
+        self.graph = defaultdict(list)
+
+    def insert_edge(self, v1, v2):
+        """Insert an edge between two vertices in the graph."""
+        self.graph[v1].append(v2)
+        # 1 => 2, 3 means 1: [2, 3]
+
+    def print_graph(self):
+        """Print the graph in a readable format."""
+        for vertex, edges in self.graph.items():
+            print(f"{vertex} => {', '.join(map(str, edges))}")
+
+
+g = DirectedGraph()
+# Example usage of the Graph class and create a cyclic graph.
+g.insert_edge(1, 2)
+g.insert_edge(1, 100)
+g.insert_edge(2, 3)
+g.insert_edge(3, 4)
+g.insert_edge(4, 1)
+
+g.print_graph()
+print("------------Directed graph created end.---------")
+
+
+class UndirectedGraph:
+    """A class to represent an undirected graph using an adjacency list."""
+
+    def __init__(self):
+        """Initialize an empty graph."""
+        self.graph = defaultdict(set)
+
+    def insert_edge(self, v1, v2):
+        """Insert an edge between two vertices in the graph."""
+        self.graph[v1].add(v2)
+        self.graph[v2].add(v1)  # Add the reverse edge for undirected graph
+
+    def print_graph(self):
+        """Print the graph in a readable format."""
+        for vertex, edges in self.graph.items():
+            print(f"{vertex} => {', '.join(map(str, edges))}")
+
+
+g_undirected = UndirectedGraph()
+# Example usage of the UndirectedGraph class.
+g_undirected.insert_edge(1, 2)
+g_undirected.insert_edge(1, 100)
+g_undirected.insert_edge(2, 3)
+g_undirected.insert_edge(3, 4)
+
+g_undirected.print_graph()
